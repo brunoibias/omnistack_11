1887fdf1feace89f1f47ad15935bafc6aa3bbdf7
Bruno Ibias <brunoibias@gmail.com>
2020-03-27 09:47:39 -0300

    validaçoes beckend

--- omnistack_11_project/beckend/src/index.js
+++ omnistack_11_project/beckend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const cors= require('cors')
+const cors = require('cors')
 const routes = require('./rotes')
+const { errors } = require('celebrate')
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
 
